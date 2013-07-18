d163d98c031cb6049f1168c007b08edff55367ae
Avner Cohen <israbirding@gmail.com>
2013-07-18 08:52:38 +0300

    Change to use Redis Time rather than client time

--- lib/switch_board/datasets/redis_dataset.rb
+++ lib/switch_board/datasets/redis_dataset.rb
@@ -32,7 +32,7 @@ module SwitchBoard
     end
 
     def register_locker(uid, name)
-      @con.sadd SWITCHBOARD_NAME, {uid: uid, name: name, created_at: Time.now}.to_json.to_s
+      @con.sadd SWITCHBOARD_NAME, {uid: uid, name: name, created_at: redis_time}.to_json.to_s
       list_lockers ## update lockers list
       true
     end
@@ -48,8 +48,9 @@ module SwitchBoard
     #Locking mechanisem is based on sorted set, sorted set is used to allow a simulation
     # of expiration time on the keys in the map
     def lock_id(locker_uid, id_to_lock, expire_in_sec = 5)
+      now = redis_time
       @con.multi do
-        @con.zadd("#{LOCK_MAP_KEY}_z", (Time.now.to_i + expire_in_sec), id_to_lock)
+        @con.zadd("#{LOCK_MAP_KEY}_z", (now + expire_in_sec), id_to_lock)
         @con.hset("#{LOCK_MAP_KEY}_h", id_to_lock, locker_uid)
       end
     end
@@ -75,12 +76,17 @@ module SwitchBoard
     private
 
     def clean_old_keys
-      keys = @con.zrangebyscore("#{LOCK_MAP_KEY}_z", 0, Time.now.to_i)
+      keys = @con.zrangebyscore("#{LOCK_MAP_KEY}_z", 0, redis_time)
       if keys.size > 0
-        @con.zremrangebyscore("#{LOCK_MAP_KEY}_z", 0, Time.now.to_i)
+        @con.zremrangebyscore("#{LOCK_MAP_KEY}_z", 0, redis_time)
         keys.each {|key| @con.hdel("#{LOCK_MAP_KEY}_h", key)}
       end
     end
+
+ def redis_time
+      instant = @con.time
+      Time.at(instant[0], instant[1]).to_i
+    end    
 
   end
 
